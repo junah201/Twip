c7d8592c681eb816622e6b3bc14012115756f2ff
junah201 <junah.dev@gmail.com>
2022-01-25 22:21:15 +0900

    feat : Twip class websocket on message

--- twip.py
+++ twip.py
@@ -1,4 +1,5 @@
 import websocket
+from json import loads
 
 class Twip():
     def __init__(self):
@@ -13,7 +14,16 @@ class Twip():
         self.ping_timeout=10
         self.ping_payload="2"
         self.events = {}
-        
+       
+    def on_message(self, wsapp, message):
+        # 0 open Sent from the server when a new transport is opened (recheck)
+        if message[0] == "0":
+            wsapp.send(message)
+        elif message[:2] == "42":
+            result = loads(message[2:])
+            if result[0] in self.events.keys():
+                self.events[result[0]](self.data_convert(result))       
+ 
     def on_ping(self,wsapp):
         wsapp.send(self.ping_payload)
         
